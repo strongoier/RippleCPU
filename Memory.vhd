fd481aa5307c285a57f3b218135ed2da732a3852
Xu Yi <xy_xuyi@foxmail.com>
2017-11-21 01:18:33 +0800

    Add Memory

---
+++ Memory.vhd
@@ -0,0 +1,38 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+entity Memory is
+    port (
+        OE: in std_logic;
+        WE: in std_logic;
+        RamAddr: out std_logic_vector(17 downto 0);
+        RamData: inout std_logic_vector(15 downto 0);
+        MemWrite: in std_logic;
+        Address: in std_logic_vector(15 downto 0);
+        WriteData: in std_logic_vector(15 downto 0);
+        ReadData: out std_logic_vector(15 downto 0)
+    );
+end Memory;
+
+architecture Behavioral of Memory is
+
+begin
+    RamAddr <= "00" & Address;
+    ReadData <= RamData;
+
+    process (OE)
+    begin
+        if rising_edge(OE) and MemWrite = '0' then
+            RamData <= (others => 'Z');
+        end if;
+    end process;
+
+    process (WE)
+    begin
+        if rising_edge(WE) and MemWrite = '1' then
+            RamData <= WriteData;
+        end if;
+    end process;
+end Behavioral;
